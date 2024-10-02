8095d2d7d40da6c376d81fca84381d990872a8a8
dtyronestrand <13547+dtyronestrand@noreply.githubuser.com>
2024-10-02 19:34:20 -0400

    styles and animation for landing page

---
+++ src/lib/slices/Hero/Geometry.svelte
@@ -0,0 +1,50 @@
+<script lang='ts'>
+    import {T as Threlte} from "@threlte/core"  ;
+    import * as THREE from 'three';
+    import {Float} from '@threlte/extras';
+    import {gsap} from 'gsap';
+    export let position: [number, number, number] = [0, 0, 0];
+    export let geometry: THREE.BufferGeometry = new THREE.IcosahedronGeometry(3);
+    export let rate = 0.5;  
+    let visible = false;
+    
+
+    const materialParams = [
+		{ color: 0x2ecc71, roughness: 0 },
+		{ color: 0xf1c40f, roughness: 0.4 },
+		{ color: 0xe74c3c, roughness: 0.1 },
+		{ color: 0x8e44ad, roughness: 0.1 },
+		{ color: 0x1abc9c, roughness: 0.1 },
+		{ color: 0x2980b9, roughness: 0, metalness: 0.5 },
+		{ color: 0x2c3e50, roughness: 0.1, metalness: 0.5 }
+	];
+
+    function getRandomMaterial(){
+        return new THREE.MeshStandardMaterial(gsap.utils.random(materialParams));    }
+    function handleClick(event: MouseEvent){
+       
+    if ('object' in event && event.object instanceof THREE.Mesh){
+        gsap.to(event.object.rotation,{
+            x: `+=${gsap.utils.random(0,3)}`,
+            y: `+=${gsap.utils.random(0,3)}`,
+            z: `+=${gsap.utils.random(0,3)}`,
+            duration: 1.3,
+            ease: 'elastic.out.out{1,0.3}',
+            yoyo:true,
+
+        });
+        
+        
+        event.object.material = getRandomMaterial();
+    }
+    }
+  
+    const bounce = 
+</script>
+<Threlte.Group position={position.map((p)=>p*2)}>
+    <Float speed={5 * rate} rotationSpeed={5*rate} rotationIntensity={6*rate} floatIntensity={5*rate}>
+<Threlte.Mesh {geometry} material={getRandomMaterial()}
+interactive on:click={handleClick}>
+</Threlte.Mesh>
+</Float>
+</Threlte.Group>
---
+++ src/lib/slices/Hero/Shapes.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+    import {T as Threlte} from "@threlte/core"  ;
+    import * as THREE from 'three';
+	import Geometry from "./Geometry.svelte";
+</script>
+
+<Threlte.PerspectiveCamera makeDefault position={[0,0,25]} aspect={1} fov={30} near={1} far={40}/>
+<Geometry position={[0,0,0]} rate={0.3} geometry={new THREE.IcosahedronGeometry(3)}/>
+<Geometry position={[1,-0.75,4]} rate={0.4} geometry={new THREE.CapsuleGeometry(0.5,1.6,2,16)}/>
+
+<Threlte.DirectionalLight position{(5, 15, 50)} intensity={25}></Threlte.DirectionalLight>
