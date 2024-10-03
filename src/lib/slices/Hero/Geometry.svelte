a098ccbe98ef63b78c104f7babf0d34b740cb2fe
dtyronestrand <134572+dtyronestrand@noreply.githubuser.com>
2024-10-02 23:06:40 -0400

    landing page shapes completed

--- src/lib/slices/Hero/Geometry.svelte
+++ src/lib/slices/Hero/Geometry.svelte
@@ -1,11 +1,18 @@
 <script lang='ts'>
     import {T as Threlte} from "@threlte/core"  ;
     import * as THREE from 'three';
-    import {Float} from '@threlte/extras';
+    import {Float, createTransition} from '@threlte/extras';
     import {gsap} from 'gsap';
+	import { elasticOut } from "svelte/easing";
     export let position: [number, number, number] = [0, 0, 0];
     export let geometry: THREE.BufferGeometry = new THREE.IcosahedronGeometry(3);
     export let rate = 0.5;  
+
+    const soundEffects = [
+        new Audio('/sounds/hit1.ogg'),
+        new Audio('/sounds/hit2.ogg'),
+        new Audio('/sounds/hit3.ogg'),
+]
     let visible = false;
     
 
@@ -22,7 +29,7 @@
     function getRandomMaterial(){
         return new THREE.MeshStandardMaterial(gsap.utils.random(materialParams));    }
     function handleClick(event: MouseEvent){
-       
+       gsap.utils.random(soundEffects).play();
     if ('object' in event && event.object instanceof THREE.Mesh){
         gsap.to(event.object.rotation,{
             x: `+=${gsap.utils.random(0,3)}`,
@@ -39,11 +46,22 @@
     }
     }
   
-    const bounce = 
+    const bounce = createTransition ((ref)=>{
+        return {
+            tick(t){
+                if (t>0) visible = true;
+                ref.scale.set(t,t,t);
+            },
+            easing: elasticOut,
+            duration: gsap.utils.random(800,1200),
+            delay: gsap.utils.random(0,500)
+        }
+    })
 </script>
+
 <Threlte.Group position={position.map((p)=>p*2)}>
     <Float speed={5 * rate} rotationSpeed={5*rate} rotationIntensity={6*rate} floatIntensity={5*rate}>
-<Threlte.Mesh {geometry} material={getRandomMaterial()}
+<Threlte.Mesh {visible} {geometry} in={bounce} material={getRandomMaterial()}
 interactive on:click={handleClick}>
 </Threlte.Mesh>
 </Float>
