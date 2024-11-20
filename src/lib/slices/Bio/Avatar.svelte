83039d10449440589425b95b7a0c94159c6a0784
dtyronestrand <1354732+dtyronestrand@noreply.githubuser.com>
2024-11-19 21:51:25 -0500

    avatar animation

--- src/lib/slices/Bio/Avatar.svelte
+++ src/lib/slices/Bio/Avatar.svelte
@@ -8,6 +8,8 @@
     let className:string = "";
     export {className as class};
     
+    let component: HTMLElement;
+
     onMount(()=>{
         gsap.fromTo('.avatar', {
             opacity:0,
@@ -18,12 +20,35 @@
         duration:1.3,
         ease: 'power3.inOut'
     });
-    });
+
+    window.onmousemove = (e) => {
+    if (!component) return;
+    const componentRect= (component as HTMLElement) .getBoundingClientRect();
+    const  componentCenterX = componentRect.left - componentRect.width / 2;
+
+let componentPercent= {x: (e.clientX - componentCenterX) / componentRect.width};
+
+let disFromCenterX = 1 - Math.abs(componentPercent.x);
+
+gsap.timeline({
+    defaults:{
+        duration:0.5, overwrite:'auto', ease:'power3.out'}
+    }).to('.avatar', {
+        rotation: gsap.utils.clamp(-2,2,5*componentPercent.x)
+    },0).to('.highlight', {
+        opacity: disFromCenterX - 0.7,
+        x: -10 + 20 * componentPercent.x
+    },0)
+}
+});
+
+
 </script>
 
-<div class={clsx(`relative h-full w-full`, className)}>
+<div class={clsx(`relative h-full w-full`, className)} bind:this={component}>
     <div class="avatar aspect-square overflow-hidden rounded-3xl border-2 border-primary-700 opacity-0">
         <PrismicImage field={image} class="avatar-image h-full w-full object-fill" imgixParams={{q:90}}/>
+        <div class="highligh absolute inset:0 w-full scale-110 bg-gradient-to-tr from-trnsparent via-white to-transparent opacity-0"></div>
     </div>
 </div>
 <style>
