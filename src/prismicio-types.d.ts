29663864ad0a142de2d4f1f806845a285431f2ac
dtyronestrand <1354732+dtyronestrand@noreply.githubuser.com>
2024-11-20 20:55:13 -0500

    start content index

--- src/prismicio-types.d.ts
+++ src/prismicio-types.d.ts
@@ -102,6 +102,7 @@ export type BlogpostDocument<Lang extends string = string> = prismic.PrismicDocu
 >;
 
 type PageDocumentDataSlicesSlice =
+	| ContentIndexSlice
 	| ExperienceSlice
 	| TechListSlice
 	| BioSlice
@@ -395,6 +396,88 @@ type BioSliceVariation = BioSliceDefault;
  * - **Documentation**: https://prismic.io/docs/slice
  */
 export type BioSlice = prismic.SharedSlice<'bio', BioSliceVariation>;
+
+/**
+ * Primary content in *ContentIndex → Default → Primary*
+ */
+export interface ContentIndexSliceDefaultPrimary {
+	/**
+	 * Heading field in *ContentIndex → Default → Primary*
+	 *
+	 * - **Field Type**: Text
+	 * - **Placeholder**: *None*
+	 * - **API ID Path**: content_index.default.primary.heading
+	 * - **Documentation**: https://prismic.io/docs/field#key-text
+	 */
+	heading: prismic.KeyTextField;
+
+	/**
+	 * Content Type field in *ContentIndex → Default → Primary*
+	 *
+	 * - **Field Type**: Select
+	 * - **Placeholder**: *None*
+	 * - **API ID Path**: content_index.default.primary.content_type
+	 * - **Documentation**: https://prismic.io/docs/field#select
+	 */
+	content_type: prismic.SelectField<'Blog' | 'Project'>;
+
+	/**
+	 * Description field in *ContentIndex → Default → Primary*
+	 *
+	 * - **Field Type**: Rich Text
+	 * - **Placeholder**: *None*
+	 * - **API ID Path**: content_index.default.primary.description
+	 * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+	 */
+	description: prismic.RichTextField;
+
+	/**
+	 * View More Text field in *ContentIndex → Default → Primary*
+	 *
+	 * - **Field Type**: Text
+	 * - **Placeholder**: *None*
+	 * - **API ID Path**: content_index.default.primary.view_more_text
+	 * - **Documentation**: https://prismic.io/docs/field#key-text
+	 */
+	view_more_text: prismic.KeyTextField;
+
+	/**
+	 * Fallback Item Image field in *ContentIndex → Default → Primary*
+	 *
+	 * - **Field Type**: Image
+	 * - **Placeholder**: *None*
+	 * - **API ID Path**: content_index.default.primary.fallback_item_image
+	 * - **Documentation**: https://prismic.io/docs/field#image
+	 */
+	fallback_item_image: prismic.ImageField<never>;
+}
+
+/**
+ * Default variation for ContentIndex Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type ContentIndexSliceDefault = prismic.SharedSliceVariation<
+	'default',
+	Simplify<ContentIndexSliceDefaultPrimary>,
+	never
+>;
+
+/**
+ * Slice variation for *ContentIndex*
+ */
+type ContentIndexSliceVariation = ContentIndexSliceDefault;
+
+/**
+ * ContentIndex Shared Slice
+ *
+ * - **API ID**: `content_index`
+ * - **Description**: ContentIndex
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type ContentIndexSlice = prismic.SharedSlice<'content_index', ContentIndexSliceVariation>;
 
 /**
  * Item in *Experience → Default → Primary → Item*
@@ -751,6 +834,10 @@ declare module '@prismicio/client' {
 			BioSliceDefaultPrimary,
 			BioSliceVariation,
 			BioSliceDefault,
+			ContentIndexSlice,
+			ContentIndexSliceDefaultPrimary,
+			ContentIndexSliceVariation,
+			ContentIndexSliceDefault,
 			ExperienceSlice,
 			ExperienceSliceDefaultPrimaryItemItem,
 			ExperienceSliceDefaultPrimary,
